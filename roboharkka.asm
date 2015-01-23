778d4c693a56820720ec5f5bebe14a64ea828483
Sami <nieminen-s@suomi24.fi>
2015-01-23 16:39:13 +0200

    First commit

---
+++ roboharkka.asm
@@ -0,0 +1,111 @@
+ 
+
+
+	list  P=PIC18F452, F=INHX32, C=160, N=0, ST=OFF, MM=OFF, R=DEC, X=ON
+
+    #include "P18F452.INC"  ; Include header file
+
+		__CONFIG  _CONFIG1H, _HS_OSC_1H  ;HS oscillator
+        __CONFIG  _CONFIG2L, _PWRT_ON_2L & _BOR_ON_2L & _BORV_42_2L  ;Reset
+        __CONFIG  _CONFIG2H, _WDT_OFF_2H  ;Watchdog timer disabled
+        __CONFIG  _CONFIG3H, _CCP2MX_ON_3H  ;CCP2 to RC1 (rather than to RB3)
+        __CONFIG  _CONFIG4L, _LVP_OFF_4L  ;RB5 enabled for I/O
+
+        CBLOCK 0x20             ; Declare variable addresses starting at 0x20
+          dataL
+        ENDC
+
+        ORG    0x000            ; Program starts at 0x000
+
+; ------------------------------------
+; SET BAUD RATE TO COMMUNICATE WITH PC
+; ------------------------------------
+; Boot Baud Rate = 9600, No Parity, 1 Stop Bit
+;
+
+
+
+       ;;;;;;HARKKA;;;
+
+        BANKSEL SPBRG
+        movlw D'25'
+        movwf SPBRG
+        BANKSEL TXSTA
+        bsf TXSTA, BRGH
+
+        BANKSEL TXSTA
+        movlw B'00100100'
+        movwf TXSTA
+        BANKSEL RCSTA
+        movlw B'10010000'
+        movwf RCSTA
+
+;
+; ------------------------------------
+; PROVIDE A SETTLING TIME FOR START UP
+; ------------------------------------
+;
+        clrf dataL
+settle  decfsz dataL,F
+        goto settle
+
+        movf RCREG,W
+        movf RCREG,W
+        movf RCREG,W            ; flush receive buffer
+;
+; ---------
+; MAIN LOOP
+; ---------
+;
+        rcall message          
+loop 
+        rcall send               ; send the char
+        goto loop
+
+; -------------------------------------------------------------
+; SEND CHARACTER IN W VIA RS232 AND WAIT UNTIL FINISHED SENDING
+; -------------------------------------------------------------
+;
+
+
+;;;;;;;;;;;;;;;HARKKA;;;;;;;
+
+send 
+      movwf TXREG
+      BANKSEL PIR1
+WaitTX
+       btfss PIR1, TXIF
+       goto WaitTX  
+       return  
+
+
+
+;
+; -------
+; MESSAGE
+; -------
+;
+message movlw  '#'
+        call send
+        movlw  '8'
+        call send
+        movlw  'P'
+        call send
+        movlw  '7'
+        call send
+        movlw  '5'
+        call send
+        movlw  '0'
+        call send
+        movlw  '0'
+        call send
+        movlw  '<'
+        call send
+        movlw  0x0D ;CR
+        call send
+        movlw  '>'
+        call send
+        return
+
+        END 
+
